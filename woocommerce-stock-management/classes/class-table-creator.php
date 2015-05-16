97f82ff3dc253718b7011105a8811260d9f8d917
Adrian Du Plessis <adrian@graphiteprogramming.com>
2015-05-16 21:35:40 +0200

    Started the template files

---
+++ woocommerce-stock-management/classes/class-table-creator.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Generic Table creation class
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'Cheaters Detected!' );
+}
+
+if ( ! class_exists( 'Table_Creator' ) ) {
+	class Table_Creator {
+
+
+		/**
+		 *
+		 */
+	}
+}

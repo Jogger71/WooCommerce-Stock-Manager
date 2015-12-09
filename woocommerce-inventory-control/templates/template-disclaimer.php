d8e49099c7f2c4736a1e22fd1bd389e343471444
Adrian Du Plessis <adrian@graphiteprogramming.com>
2015-12-09 09:42:10 +0200

    Added basic disclaimer.

---
+++ woocommerce-inventory-control/templates/template-disclaimer.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Disclaimer information
+ * @since 0.3.0
+ */
+
+?>
+
+<div class="wrap">
+	<h2>Disclaimer</h2>
+	<p>
+		The WooCommerce Inventory Control plugin (<strong>"this plugin"</strong>) is aimed at providing a service to
+		possibly help you, running a WooCommerce online shop manage your stock better. This plugin does not come with
+		any warranty that it will do as intended and may break. Because you are using the plugin on your server and the
+		data is hosted with you we cannot guarantee that all information will always be correct.
+	</p>
+	<p>
+		This plugin is not meant to replace any stock management or accounting software. As this is not an accounting
+		program, missing information can be found on elements like purchase orders and sales orders or invoices. Please
+		ensure, before using the plugin, that what is offered will work as you needed it to.
+	</p>
+</div>
